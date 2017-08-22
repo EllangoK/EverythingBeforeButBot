cc6bd906ea599a6fbdb23b568b6ea1b9f3854718
EllangoK <ellangok@bxscience.edu>
2017-08-21 21:32:54 -0400

    Added logs
    Added logs so it doesnt reply twice to the same thing.

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,15 +1,28 @@
 import praw
 import re
+import pdb
+import os
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("ArkunEnterprises")
-pizza = 'pizza'
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+else:
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
 
 
 for comment in subreddit.stream.comments():
-    print(comment.body)
-    if re.search(" but " or ", but " and not "Everything before but doesn't count", comment.body, re.IGNORECASE):
+    if comment.id not in posts_replied_to:
+        if re.search(" but " or ", but " and not "Everything before but doesn't count", comment.body, re.IGNORECASE):
             comment.reply("Everything before but doesn't count. \n\n You meant this: \n\n >" + comment.body)
-            print(pizza)
+            posts_replied_to.append(comment.id)
+            with open("posts_replied_to.txt", "w") as f:
+                for post_id in posts_replied_to:
+                    f.write(post_id + "\n")
+                    print ("s")
+
             
 
