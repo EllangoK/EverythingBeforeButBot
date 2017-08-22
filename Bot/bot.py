0df35147b963e701213805a87b5c8bc8d19aa2aa
EllangoK <ellangok@bxscience.edu>
2017-08-21 23:20:14 -0400

    or to and
    Orand? Orange.

--- Bot/bot.py
+++ Bot/bot.py
@@ -20,7 +20,7 @@ def doStuff():
         if comment.id not in posts_replied_to:
             if comment.author != "EverythingBeforeBut":
                 if re.search(" but " or ", but ", comment.body, re.IGNORECASE):
-                    fixed_comment = comment.body.split(" but" or " bUT" or " buT" or " bUt" or " But" or " BUt" or " BUT" or " BuT")[0]
+                    fixed_comment = comment.body.split(" but" and " bUT" and " buT" and " bUt" and " But" and " BUt" and " BUT" and " BuT")[0]
                     comment.reply('Everything before but doesn\'t count. \n\n You meant this: \n\n >' + fixed_comment)
                     posts_replied_to.append(comment.id)
                     with open("posts_replied_to.txt", "w") as f:
@@ -33,12 +33,12 @@ try:
 except praw.exceptions.APIException:
     print ("Stuff not done")
 
-time.sleep(660) #11 mins
+#time.sleep(660) #11 mins
 
-try:
-    doStuff()
-    print ("Stuff done")
-except praw.exceptions.APIException:
-    print ("Stuff not done")
+#try:
+#    doStuff()
+#    print ("Stuff done")
+#except praw.exceptions.APIException:
+#    print ("Stuff not done")
 
 print("kid")
