09145df97099db69496e9b82bfa1fd161f8a73b6
EllangoK <ellangok@bxscience.edu>
2017-08-21 22:20:40 -0400

    Gave it the ability to have friends
    Not insane anymore

--- Bot/bot.py
+++ Bot/bot.py
@@ -2,6 +2,7 @@ import praw
 import re
 import pdb
 import os
+import time
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("ArkunEnterprises")
@@ -13,16 +14,14 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
 
-
-for comment in subreddit.stream.comments():
-    if comment.id not in posts_replied_to:
-        if re.search(" but " or ", but " and not "Everything before but doesn't count", comment.body, re.IGNORECASE):
-            comment.reply("Everything before but doesn't count. \n\n You meant this: \n\n >" + comment.body)
-            posts_replied_to.append(comment.id)
-            with open("posts_replied_to.txt", "w") as f:
-                for post_id in posts_replied_to:
-                    f.write(post_id + "\n")
-                    print ("s")
-
-            
-
+def doStuff():
+    for comment in subreddit.stream.comments():
+        if comment.id not in posts_replied_to:
+            if comment.author != "EverythingBeforeBut":
+                if re.search(" but " or ", but " and not "Everything before but doesn't count", comment.body, re.IGNORECASE):
+                    comment.reply('Everything before but doesn\'t count. \n\n You meant this: \n\n >' + comment.body)
+                    posts_replied_to.append(comment.id)
+                    with open("posts_replied_to.txt", "w") as f:
+                        for post_id in posts_replied_to:
+                            f.write(post_id + "\n")
+                        print ("s")
