077c57f4be72cc2e54404a12399044543f469a08
EllangoK <ellangok@bxscience.edu>
2017-08-21 20:38:15 -0400

    Some more basic changes including new line
    Just need to add the everything before logic

--- Bot/bot.py
+++ Bot/bot.py
@@ -8,8 +8,8 @@ pizza = 'pizza'
 
 for comment in subreddit.stream.comments():
     print(comment.body)
-    if re.search(" but " or ", but " , comment.body, re.IGNORECASE):
-            comment.reply("test reply please ignore")
+    if re.search(" but " or ", but " and not "Everything before but doesn't count", comment.body, re.IGNORECASE):
+            comment.reply("Everything before but doesn't count. \n\n You meant this: \n\n >" + comment.body)
             print(pizza)
             
 
