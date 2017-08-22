fac56ee1c186b15d61337c925c93af684f043e22
EllangoK <ellangok@bxscience.edu>
2017-08-21 23:32:50 -0400

    Replaced and with re
    Reand? Reant? React.
    Re = Regex

--- Bot/bot.py
+++ Bot/bot.py
@@ -20,13 +20,12 @@ def doStuff():
         if comment.id not in posts_replied_to:
             if comment.author != "EverythingBeforeBut":
                 if re.search(" but " or ", but ", comment.body, re.IGNORECASE):
-                    fixed_comment = comment.body.split(" but" and " bUT" and " buT" and " bUt" and " But" and " BUt" and " BUT" and " BuT")[0]
-                    comment.reply('Everything before but doesn\'t count. \n\n You meant this: \n\n >' + fixed_comment)
+                    fixed_comment = re.split("but", comment.body, flags=re.IGNORECASE)[0]
+                    comment.reply('Everything before **but** doesn\'t count. \n\n You meant this: \n\n >' + fixed_comment)
                     posts_replied_to.append(comment.id)
                     with open("posts_replied_to.txt", "w") as f:
                         for post_id in posts_replied_to:
                             f.write(post_id + "\n")
-                        print ("Stuff Done")
 try:
     doStuff()
     print ("Stuff done")
